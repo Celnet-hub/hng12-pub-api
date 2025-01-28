6640a82371d1fff221398809143527c2ac838431
Chidubem <dubemnwabuisi@gmail.com>
2025-01-28 21:50:54 +0000

    added redirection

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 from datetime import datetime
 
 app = FastAPI()
@@ -7,16 +8,19 @@ app = FastAPI()
 # Set up CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["localhost"],  # allows all origins
     allow_credentials=True,
     allow_methods=["GET"],
-    # # allow_headers=["*"],  # Allows all headers
 )
 
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/info")
+
 @app.get("/info")
-async def read_root():
+async def get_info():
     return {
         "email": "dubemnwabuisi@gmail.com",
         "timestamp": datetime.now().isoformat(),
-        "github_url": "https://github.com/yourusername/your-repo"
+        "github_url": "https://github.com/Celnet-hub/hng12-pub-api"
     }
